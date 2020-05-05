14e161235db4fa34a944fc51a1bd9260335dd5b5
Andy Weston <aweston@westonsoftware.com>
2020-05-05 10:23:01 -0400

    WIP

---
+++ LottieSharp.Avalonia.Sample/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using LottieSharp.Avalonia.Sample.ViewModels;
+using LottieSharp.Avalonia.Sample.Views;
+
+namespace LottieSharp.Avalonia.Sample
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
